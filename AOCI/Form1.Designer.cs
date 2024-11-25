33feeaa7da7571b564fda6fe1826cd2015b7bc5a
i.sologub <i.sologub@ADMINLAPTOP>
2024-11-26 00:17:05 +0700

    Downgrade 4.9 to 4.4 Emgu.VC. This work with open image

--- AOCI/Form1.Designer.cs
+++ AOCI/Form1.Designer.cs
@@ -32,7 +32,14 @@
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
             this.DefaultImage = new Emgu.CV.UI.ImageBox();
             this.SelectImageBtn = new System.Windows.Forms.Button();
+            this.ReImage = new Emgu.CV.UI.ImageBox();
+            this.ProcessBtn = new System.Windows.Forms.Button();
+            this.CannyFirst = new System.Windows.Forms.TrackBar();
+            this.CannySecond = new System.Windows.Forms.TrackBar();
             ((System.ComponentModel.ISupportInitialize)(this.DefaultImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ReImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CannyFirst)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CannySecond)).BeginInit();
             this.SuspendLayout();
             // 
             // imageList1
@@ -57,19 +64,64 @@
             this.SelectImageBtn.TabIndex = 3;
             this.SelectImageBtn.Text = "Open Image";
             this.SelectImageBtn.UseVisualStyleBackColor = true;
+            this.SelectImageBtn.Click += new System.EventHandler(this.SelectImageBtn_Click);
+            // 
+            // ReImage
+            // 
+            this.ReImage.Location = new System.Drawing.Point(744, 12);
+            this.ReImage.Name = "ReImage";
+            this.ReImage.Size = new System.Drawing.Size(640, 480);
+            this.ReImage.TabIndex = 4;
+            this.ReImage.TabStop = false;
+            // 
+            // ProcessBtn
+            // 
+            this.ProcessBtn.Location = new System.Drawing.Point(665, 246);
+            this.ProcessBtn.Name = "ProcessBtn";
+            this.ProcessBtn.Size = new System.Drawing.Size(75, 23);
+            this.ProcessBtn.TabIndex = 5;
+            this.ProcessBtn.Text = "Process";
+            this.ProcessBtn.UseVisualStyleBackColor = true;
+            this.ProcessBtn.Click += new System.EventHandler(this.ProcessBtn_Click);
+            // 
+            // CannyFirst
+            // 
+            this.CannyFirst.LargeChange = 1;
+            this.CannyFirst.Location = new System.Drawing.Point(663, 114);
+            this.CannyFirst.Maximum = 256;
+            this.CannyFirst.Name = "CannyFirst";
+            this.CannyFirst.Size = new System.Drawing.Size(75, 45);
+            this.CannyFirst.TabIndex = 6;
+            // 
+            // CannySecond
+            // 
+            this.CannySecond.LargeChange = 1;
+            this.CannySecond.Location = new System.Drawing.Point(663, 177);
+            this.CannySecond.Maximum = 256;
+            this.CannySecond.Name = "CannySecond";
+            this.CannySecond.Size = new System.Drawing.Size(75, 45);
+            this.CannySecond.TabIndex = 7;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.ClientSize = new System.Drawing.Size(1405, 514);
+            this.Controls.Add(this.CannySecond);
+            this.Controls.Add(this.CannyFirst);
+            this.Controls.Add(this.ProcessBtn);
+            this.Controls.Add(this.ReImage);
             this.Controls.Add(this.SelectImageBtn);
             this.Controls.Add(this.DefaultImage);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.DefaultImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ReImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CannyFirst)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.CannySecond)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -78,6 +130,10 @@
         private System.Windows.Forms.ImageList imageList1;
         private Emgu.CV.UI.ImageBox DefaultImage;
         private System.Windows.Forms.Button SelectImageBtn;
+        private Emgu.CV.UI.ImageBox ReImage;
+        private System.Windows.Forms.Button ProcessBtn;
+        private System.Windows.Forms.TrackBar CannyFirst;
+        private System.Windows.Forms.TrackBar CannySecond;
     }
 }
 
--- AOCI/Form1.cs
+++ AOCI/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Emgu.CV;
 using Emgu.CV.CvEnum;
 using Emgu.CV.Structure;
+using Emgu.CV.UI;
 using Emgu.CV.Util;
 
 namespace AOCI
@@ -23,6 +24,45 @@ namespace AOCI
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private Image<Bgr, byte> sourceImage; //глобальная переменная
+
+        private void SelectImageBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                OpenFileDialog openFileDialog = new OpenFileDialog();
+                var result = openFileDialog.ShowDialog(); // открытие диалога выбора файла
+                if (result == DialogResult.OK) // открытие выбранного файла
+                {
+                    string fileName = openFileDialog.FileName;
+                    sourceImage = new Image<Bgr, byte>(fileName);
+                }
+                DefaultImage.Image = sourceImage.Resize(640, 480, Inter.Linear);
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+        }
+
+        private void ProcessBtn_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> grayImage = sourceImage.Convert<Gray, byte>();
+            var tempImage = grayImage.PyrDown();
+            var destImage = tempImage.PyrUp();
+            
+            double cannyThreshold = CannyFirst.Value;
+            double cannyThresholdLinking = CannySecond.Value;
+            
+            Image<Gray, byte> cannyEdges = destImage.Canny(cannyThreshold, cannyThresholdLinking);
+            
+            ReImage.Image = sourceImage.Resize(640, 480, Inter.Linear);
 
         }
     }
