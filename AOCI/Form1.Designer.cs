945c7af5ed64a474d646da06fd3b89b41908a35d
i.sologub <i.sologub@ADMINLAPTOP>
2024-11-25 22:49:55 +0700

    Добавьте файлы проекта.

---
+++ AOCI/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace AOCI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.DefaultImage = new Emgu.CV.UI.ImageBox();
+            this.SelectImageBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DefaultImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // DefaultImage
+            // 
+            this.DefaultImage.Location = new System.Drawing.Point(12, 12);
+            this.DefaultImage.Name = "DefaultImage";
+            this.DefaultImage.Size = new System.Drawing.Size(640, 480);
+            this.DefaultImage.TabIndex = 2;
+            this.DefaultImage.TabStop = false;
+            // 
+            // SelectImageBtn
+            // 
+            this.SelectImageBtn.Location = new System.Drawing.Point(663, 12);
+            this.SelectImageBtn.Name = "SelectImageBtn";
+            this.SelectImageBtn.Size = new System.Drawing.Size(75, 23);
+            this.SelectImageBtn.TabIndex = 3;
+            this.SelectImageBtn.Text = "Open Image";
+            this.SelectImageBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 514);
+            this.Controls.Add(this.SelectImageBtn);
+            this.Controls.Add(this.DefaultImage);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DefaultImage)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList imageList1;
+        private Emgu.CV.UI.ImageBox DefaultImage;
+        private System.Windows.Forms.Button SelectImageBtn;
+    }
+}
+
